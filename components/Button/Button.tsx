4b129289d9be24a0fef23636fe2d6ff6c8c7c0bc
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-07-04 15:26:41 +0900

    Style: 기본 버튼 색상 수정

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -7,16 +7,16 @@ const sizeClasses = {
 };
 
 const styleClasses = {
-  dark: 'bg-brand-nomad-black text-white',
-  bright: 'bg-white text-brand-nomad-black border border-brand-nomad-black',
+  dark: 'bg-nomad-black text-white',
+  bright: 'bg-white text-nomad-black border border-nomad-black',
   disabled: 'bg-var-gray6 text-white',
 };
 
 /* 기본 버튼 - 로그인하기, 신청 불가 등 기본적인 형태의 버튼
 
    size, style, onClick, children 필수 입력, disabled 선택 입력
-   size 옵션 - (small | medium | large - width 100%)
-   style 옵션 - (dark-어두운 배경에 흰글씨 | bright - 흰 배경에 어두운 글씨 | disabled - 회색 배경에 흰 글씨)
+   size 옵션 - small | medium | large - width 100%
+   style 옵션 - dark-어두운 배경에 흰글씨 | bright - 흰 배경에 어두운 글씨 | disabled - 회색 배경에 흰 글씨
    사용 예시 <Button size="small" style="dark" onClick={handleClick}>로그인하기</Button> */
 
 export function PrimaryButton({
