1ac6aa5fdf5874348514ea160939df46c08bedbb
Park Sumin <fhsjdvs@gmail.com>
2024-07-04 17:59:09 +0900

    Feat: 반응형 구현

--- components/SearchBar/Searchbar.tsx
+++ components/SearchBar/Searchbar.tsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Icon from '@/public/icon/icon_search.svg';
 
+/*
+메인화면에서 보이는 검색 바 입니다.
+*/
+
 function SearchBar() {
   const [inputValue, setInputValue] = useState<string>('');
 
@@ -10,14 +14,14 @@ function SearchBar() {
   };
 
   return (
-    <div className="flex flex-col gap-[32px]">
-      <span className="font-sans text-[20px] font-[700]">
+    <div className="p:w-[1200px] t:w-[696px] m:w-[343px] p:h-[178px] t:h-[166px] m:p-[130px] flex flex-col p:gap-[32px] t:gap-[20px] m:gap-[15px] bg-white p-[32px_24px_32px_24px] m:p-[16px_24px_16px_24px] rounded-2xl">
+      <span className="font-sans text-[20px] m:text-[16px] font-[700]">
         무엇을 체험하고 싶으신가요?
       </span>
       <div className="flex gap-[12px]">
-        <div className="relative w-[1000px] h-[56px]">
+        <div className="relative p:w-[1000px] h-[56px] t:w-[500px] m:w-[188px]">
           <input
-            className=" border border-[#79747E] border-solid w-[1000px] h-[56px] flex p-[4px_16px_4px_40px] items-center self-stretch rounded font-sans text-[16px] font-[400]"
+            className=" border border-[#79747E] border-solid p:w-[1000px] t:w-[500px] m:w-[188px] h-[56px] flex p-[4px_16px_4px_40px] items-center self-stretch rounded font-sans text-[16px] m:text-[14px] font-[400]"
             placeholder="내가 원하는 체험은"
             onChange={handleChange}
           ></input>
@@ -29,14 +33,14 @@ function SearchBar() {
             alt="검색 아이콘"
           ></Image>
           {inputValue ? (
-            <div className="w-[124px] h-[26px] text-[16px] font-sans font-[400] text-[#A4A1AA] absolute top-[-10px] left-10 bg-white text-center">
+            <div className="w-[124px] h-[26px] text-[16px] font-sans font-[400] text-var-gray6 absolute top-[-10px] left-10 bg-white text-center">
               내가 원하는 체험은
             </div>
           ) : (
             <div></div>
           )}
         </div>
-        <button className="w-[136px] h-[56px] flex items-center justify-center bg-[#121] text-[white] font-sans font-[700] text-[16px] leading-7 rounded">
+        <button className="w-[136px] m:w-[96px] h-[56px] flex items-center justify-center bg-nomad-black text-[white] font-sans font-[700] text-[16px] leading-7 rounded">
           검색하기
         </button>
       </div>
--- pages/Test.tsx
+++ pages/Test.tsx
@@ -1,5 +1,9 @@
 import SearchBar from '@/components/SearchBar/Searchbar';
 
 export default function Test() {
-  return <SearchBar></SearchBar>;
+  return (
+    <div className="w-screen h-screen bg-gray-100 p-[20px]">
+      <SearchBar></SearchBar>
+    </div>
+  );
 }
