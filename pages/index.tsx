1d28bf95851218ce1c93021eaae6b656be125d1c
Jisung Jeong <ssjeond@naver.com>
2024-07-04 18:01:38 +0900

    docs: 컴포넌트 테스트 페이지 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,3 @@
-import CatergoryBtn from '@/components/CatergoryBtn/CatergoryBtn';
-import NavigationBar from '@/components/NavigationBar/NavigationBar';
-import PriceFilterBtn from '@/components/PriceFilterBtn/PriceFilterBtn';
-
 export default function Home() {
-  return (
-    <>
-      <NavigationBar /> <CatergoryBtn categoryName="버튼이름 예시" />{' '}
-      <PriceFilterBtn />
-    </>
-  );
+  return <div>프로젝트 초기 설정</div>;
 }
