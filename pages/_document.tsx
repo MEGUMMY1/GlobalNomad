1f89a4a563fb2f7320625054a684b92841558f4e
MEGUMMY1 <cys10902@nate.com>
2024-07-05 22:42:23 +0900

    Feat: 예약 상세 화면 지도(Kakao API 사용)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { appKey } from '@/static/appKey';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -5,6 +6,10 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <title>GlobalNomad</title>
+        <script
+          type="text/javascript"
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${appKey}&libraries=services,clusterer,drawing`}
+        ></script>
       </Head>
       <body>
         <Main />
