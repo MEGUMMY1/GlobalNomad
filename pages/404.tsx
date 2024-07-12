f1540d1a7586d60b19d5c4c79784c056ecc81865
MEGUMMY1 <cys10902@nate.com>
2024-07-12 17:06:25 +0900

    Feat: 404(Not Found) page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+import { PrimaryButton } from '@/components/Button/Button';
+import { useRouter } from 'next/router';
+
 export default function NonePage() {
-  return <div>없는페이지 입니다.</div>;
+  const router = useRouter();
+
+  const handleToMain = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className="flex items-center flex-col mt-14 m:mt-10">
+      <div className="w-[500px] m:w-[300px]">
+        <Image src="/image/404.png" width={500} height={500} alt="notFound" />
+        <PrimaryButton size="large" style="dark" onClick={handleToMain}>
+          메인으로 이동
+        </PrimaryButton>
+      </div>
+    </div>
+  );
 }
