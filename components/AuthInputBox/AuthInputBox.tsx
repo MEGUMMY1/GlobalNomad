eaf939b11b3316607d3645e24a86204b4b03a21f
김보민 <bomin@Bomin-MacBook.local>
2024-07-19 04:35:19 +0900

    Fix: 비밀번호 보기 버튼과 인풋 영역 분리

--- components/AuthInputBox/AuthInputBox.tsx
+++ components/AuthInputBox/AuthInputBox.tsx
@@ -32,34 +32,37 @@ export default function AuthInputBox({
   return (
     <div className="flex flex-col gap-[8px] text-[16px] relative">
       <label htmlFor={name}>{label}</label>
-      <input
-        id={name}
-        className={`border ${errors[name] ? 'border-var-red2' : ''} py-[16px] px-[20px] rounded-md border-var-gray6`}
-        type={isEyeOpen ? 'text' : type}
-        placeholder={placeholder}
-        {...register(name, validation)}
-      />
+      <div
+        className={`${errors[name] ? 'border-red-500' : ''} border border-var-gray6 border-solid w-full py-[16px] px-[20px] rounded-md flex items-center relative`}
+      >
+        <input
+          id={name}
+          className="w-full border-none outline-none"
+          type={isEyeOpen ? 'text' : type}
+          placeholder={placeholder}
+          {...register(name, validation)}
+        />
+        {/* 비밀번호 보기 버튼 */}
+        {eyeIconActive && (
+          <button type="button" onClick={handleClick}>
+            <Image
+              width={24}
+              height={24}
+              src={
+                isEyeOpen
+                  ? '/icon/btn_visibility_on_.svg'
+                  : '/icon/btn_visibility_off.png'
+              }
+              alt="비밀번호_확인_버튼"
+            />
+          </button>
+        )}
+      </div>
       {/* 에러 메세지 */}
       {errors[name] && (
-        <span className="text-[12px] text-var-red2">
+        <span className="text-[12px] text-red-500">
           {errors[name]?.message}
         </span>
-      )}
-      {/* 비밀번호 보기 버튼 */}
-      {eyeIconActive && (
-        <button type="button" onClick={handleClick}>
-          <Image
-            width={24}
-            height={24}
-            className="absolute right-[20px] top-[50px]"
-            src={
-              isEyeOpen
-                ? '/icon/btn_visibility_on_.svg'
-                : '/icon/btn_visibility_off.png'
-            }
-            alt="비밀번호_확인_버튼"
-          />
-        </button>
       )}
     </div>
   );
