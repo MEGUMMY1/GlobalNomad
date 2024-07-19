b761c54d8f1eb9a5aee51e4aa7edf1fe5781fe69
Jisung Jeong <ssjeond@naver.com>
2024-07-19 18:17:13 +0900

    fix: 모바일 알림창 무한 스크롤 버그 해결

--- components/Layout/NavigationBar.tsx
+++ components/Layout/NavigationBar.tsx
@@ -73,7 +73,7 @@ export default function NavigationBar() {
                 <div className="relative">
                   <Image src={notificationIcon} alt="알림 아이콘" />
                   {data?.totalCount !== undefined && data?.totalCount > 0 && (
-                    <span className="flex justify-center absolute -top-2 -right-2 bg-red-500 w-[15px] h-[15px] text-white text-xs rounded-full px-2">
+                    <span className="flex justify-center items-center absolute -top-2 -right-2 bg-red-500 w-[15px] h-[15px] text-white text-xs rounded-full px-2">
                       {data.totalCount}
                     </span>
                   )}
