2a4b9d62687eab9ca958ed8d89b2cc5dab59651a
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-07-20 16:15:46 +0900

    Style: 시간 드롭다운 다크모드 시 색상 변경

--- components/MyActivity/Register/TimeDropdown.tsx
+++ components/MyActivity/Register/TimeDropdown.tsx
@@ -58,12 +58,12 @@ function TimeDropdown({
         />
       </button>
       {isOpen && (
-        <ul className="absolute z-10 animate-slideDown p:w-[140px] w-full h-[200px] rounded-[8px] overflow-y-scroll scrollbar-hide m:w-full">
+        <ul className="absolute z-10 animate-slideDown mt-[4px] p:w-[140px] w-full h-[200px] rounded-[8px] overflow-y-scroll scrollbar-hide m:w-full">
           {times.map((time) => (
             <li
               key={time}
               onClick={() => handleSelectTime(time)}
-              className="h-[56px] text-var-black py-[18px] px-[16px] m:px-[12px] hover:bg-gray-100 cursor-pointer bg-white dark:bg-var-dark2 dark:text-var-gray2 dark:hover:bg-var-gray2 dark:hover:text-var-dark2"
+              className="h-[56px] text-var-black py-[18px] px-[16px] m:px-[12px] hover:bg-gray-100 cursor-pointer bg-white dark:bg-var-dark3 dark:text-var-gray2 dark:hover:bg-var-gray2 dark:hover:text-var-dark3"
             >
               {time}
             </li>
