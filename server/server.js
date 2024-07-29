774e9f945e9ebc671ffbc8cd83d0216345eaaabb
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-07-29 13:47:10 +0900

    Fix: 서버 주소 변경

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
-const socket = io('http://localhost:5001');
+const socket = io('https://rear-ninnetta-codeitsprinter-6a0e09e4.koyeb.app/');
 
 export default socket;
