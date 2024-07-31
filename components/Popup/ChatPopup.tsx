1b09e8f825097ee3019e2bb377ad60c83259a0b6
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-07-31 23:05:59 +0900

    Revert "Revert "Feat: 채팅 팝업 기능 추가""

--- components/Popup/ChatPopup.tsx
+++ components/Popup/ChatPopup.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { CloseButtonBold } from '../Button/Button';
 import {
   ChatListProps,
   ChatPopupProps,
@@ -13,11 +12,23 @@ import Image from 'next/image';
 function ChatPopup({
   closePopup,
   activityId,
+  activityTitle,
+  activityImage,
   isAdmin = false,
 }: ChatPopupProps) {
   const [message, setMessage] = useState('');
   const [senderId, setSenderId] = useState(0);
+  const [nickname, setNickName] = useState('문의 하기');
+  const [profile, setProfile] = useState(activityImage);
   const [isSendEnabled, setIsSendEnabled] = useState(true);
+  const [isEnter, setIsEnter] = useState(false);
+
+  const handleClickPrev = () => {
+    setIsEnter(false);
+    setIsSendEnabled(false);
+    setNickName('문의 내역');
+    setProfile(activityImage);
+  };
 
   const sendMessage = (event: any) => {
     event.preventDefault();
@@ -43,15 +54,50 @@ function ChatPopup({
 
   useEffect(() => {
     setIsSendEnabled(!isAdmin);
+    if (isAdmin) {
+      setNickName('문의 내역');
+    }
   }, []);
 
   return (
-    <div className="flex items-center justify-center fixed w-[400px] bottom-[30px] right-[30px] z-50">
-      <div className="flex flex-col bg-var-gray2 w-full rounded-[20px]">
-        <div className="flex justify-between pt-[7px] pb-[3px] px-[20px] items-center bg-var-gray3 rounded-t-[20px]">
-          <p>문의 채팅</p>
+    <div className="flex items-center justify-center fixed w-[400px] bottom-[30px] right-[30px] z-50 m:inset-0 m:w-full m:bg-black m:bg-opacity-70">
+      <div className="flex flex-col bg-var-gray2 w-full rounded-[20px] dark:bg-var-dark2 dark:border-4 dark:border-solid dark:border-var-dark3">
+        <div className="flex justify-between h-[64px] pt-[10px] pb-[3px] px-[20px] items-center bg-var-gray3 rounded-t-[15px] dark:bg-var-dark3">
+          <div className="flex gap-[10px]">
+            <div className="w-[40px] h-[40px] rounded-full">
+              <Image
+                src={profile}
+                alt="프로필"
+                width={40}
+                height={40}
+                className="w-[40px] h-[40px] rounded-full"
+              />
+            </div>
+            <div>
+              <p className="font-[500] line-clamp-1">{activityTitle}</p>
+              <p className="text-var-gray6">{`@${nickname}`}</p>
+            </div>
+          </div>
+
           <div className="flex items-center h-[30px]">
-            <CloseButtonBold onClick={closePopup} />
+            {isEnter && (
+              <button onClick={handleClickPrev}>
+                <Image
+                  src="/icon/prev_arrow.svg"
+                  alt="이전"
+                  width={20}
+                  height={20}
+                />
+              </button>
+            )}
+            <button onClick={closePopup}>
+              <Image
+                src="/icon/chat_close.svg"
+                alt="닫기"
+                width={24}
+                height={24}
+              />
+            </button>
           </div>
         </div>
         <div className="flex flex-col h-[500px]">
@@ -60,6 +106,10 @@ function ChatPopup({
               activityId={activityId}
               handleSenderId={setSenderId}
               handleSendEnable={setIsSendEnabled}
+              isEnter={isEnter}
+              handleIsEnter={setIsEnter}
+              handleNickName={setNickName}
+              handleProfile={setProfile}
             />
           ) : (
             <ShowChatList />
@@ -91,18 +141,23 @@ function ShowChatRoomList({
   activityId,
   handleSenderId,
   handleSendEnable,
+  isEnter,
+  handleIsEnter,
+  handleNickName,
+  handleProfile,
 }: ChatRoomPopupProps) {
   const [rooms, setRooms] = useState<ChatRoomProps[]>([]);
-  const [isEnter, setIsEnter] = useState(false);
   const { userData } = useUserData();
 
-  const handleClickRoom = (userId: number) => {
+  const handleClickRoom = (userId: number, index: number) => {
     socket.emit('inquiryAdmin', userData.id, activityId, userId, (res: any) => {
       console.log('inquiryAdmin res', res);
     });
-    setIsEnter(true);
+    handleIsEnter(true);
     handleSenderId(userId);
     handleSendEnable(true);
+    handleNickName(rooms[index].user.name);
+    handleProfile(rooms[index].user.profile);
   };
 
   useEffect(() => {
@@ -129,8 +184,8 @@ function ShowChatRoomList({
               <div key={room.user.id} className="flex justify-center">
                 <button
                   type="button"
-                  onClick={() => handleClickRoom(room.user.id)}
-                  className={`flex items-center gap-[15px] p-[15px] h-[75px] bg-var-gray2 w-full max-w-[400px] border-b border-solid border-b-var-gray4`}
+                  onClick={() => handleClickRoom(room.user.id, index)}
+                  className={`flex items-center gap-[15px] p-[15px] h-[75px] bg-var-gray2 w-full max-w-[400px] border-b border-solid border-b-var-gray4 dark:bg-var-dark2`}
                 >
                   <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
                     <Image
@@ -141,7 +196,7 @@ function ShowChatRoomList({
                     />
                   </div>
                   <div className="flex flex-col items-start">
-                    <div className="font-[500] text-nomad-black">
+                    <div className="font-[500] text-nomad-black dark:text-var-gray2">
                       {room.user.name}
                     </div>
                     <div className="text-var-gray6">
@@ -153,7 +208,7 @@ function ShowChatRoomList({
             ))}
           </div>
         ) : (
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center mt-[20px]">
             <p>아직 문의 채팅이 없습니다.</p>
           </div>
         ))}
@@ -200,7 +255,7 @@ function ShowChatList({ isAdmin = false }: ChatListProps) {
               className={`inline-block max-w-max rounded-[10px] p-[8px] pl-[10px] ${
                 sender[index] === 'user'
                   ? 'bg-var-green2 text-white'
-                  : 'bg-var-gray3'
+                  : 'bg-var-gray3 text-var-dark1'
               }`}
             >
               {message}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import io from 'socket.io-client';
 const socket = io('https://rear-ninnetta-codeitsprinter-6a0e09e4.koyeb.app/');
+// const socket = io('http://localhost:5001');
 
 export default socket;
