eed71529f5850ab9d5214346552883983a2fa093
Park Sumin <fhsjdvs@gmail.com>
2024-08-01 23:55:59 +0900

    Style: 방문 목록 보더색상 변경

--- components/ViewedActivities/ViewedActivities.tsx
+++ components/ViewedActivities/ViewedActivities.tsx
@@ -56,7 +56,7 @@ function ViewedActivities() {
       </div>
       {isOpen && (
         <div
-          className={`w-[180px] h-[300px] rounded-lg fixed right-[32px] m:right-[14px] bottom-[138px] z-30 bg-white border-solid border-4 border-gray-800 ${faded} flex flex-col items-center px-[10px] pt-[10px] gap-[10px] overflow-y-auto overflow-x-hidden pb-[10px] custom-scrollbar dark:border-white dark:bg-gray-900`}
+          className={`w-[180px] h-[300px] rounded-lg fixed right-[32px] m:right-[14px] bottom-[138px] z-30 bg-white border-solid border-4 border-gray-800 ${faded} flex flex-col items-center px-[10px] pt-[10px] gap-[10px] overflow-y-auto overflow-x-hidden pb-[10px] custom-scrollbar dark:border-gray-400 dark:bg-gray-900`}
         >
           <div className="font-sans text-[14px] font-[600]">
             최근 방문한 체험
